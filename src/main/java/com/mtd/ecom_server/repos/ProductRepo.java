3d1f9216b7baa166da4f93ed3d877d59b8dc58d6
Deena Kiran MK <2023ci_deenakiranmk_a@nie.ac.in>
2025-09-19 14:58:33 +0530

    Delete src directory

--- src/main/java/com/mtd/ecom_server/repos/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.mtd.ecom_server.repos;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.mtd.ecom_server.models.Product;
-
-public interface ProductRepo extends MongoRepository<Product, String> {
-
-}
